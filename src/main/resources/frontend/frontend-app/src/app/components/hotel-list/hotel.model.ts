63e5f97bf3fa9fc64310b36ed485408ce2b7ff0b
semram <semra_511@hotmail.com>
2020-03-23 15:21:26 +0100

    HOTEL-001D Added hotel overview for list [MS,SM]

---
+++ src/main/resources/frontend/frontend-app/src/app/components/hotel-list/hotel.model.ts
@@ -0,0 +1,30 @@
+export class Hotel {
+    public name: string;
+    public description: string;
+    public price: string;
+    public imagePath: string;
+    public stars: number;
+    public rate: number;
+    public city: string;
+  
+    constructor(name: string, description: string, price: string, imagePath: string, stars: number, rate: number, city: string){
+      this.name = name;
+      this.description = description;
+      this.price = price;
+      this.imagePath = imagePath;
+      this.stars = stars;
+      this.rate = rate;
+      this.city = city;
+    }
+  
+    getRating() {
+      if(this.rate <= 10 && this.rate >= 8) {
+        return "Excellent";
+      }
+      else if(this.rate < 8 && this.rate >= 5) {
+        return "Good"
+      }
+      else return "Poor";
+    }
+  }
+  
