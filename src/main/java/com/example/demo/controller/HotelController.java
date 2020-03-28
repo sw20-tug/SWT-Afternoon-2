c9f611438a3213fc48a1c5eea5878a0a8a84947d
Aleksandar Stojicic <stojicic@student.tugraz.at>
2020-03-28 20:08:49 +0100

    HOTEL_002A Implemented price filtering in Controller [MK, AS]

--- src/main/java/com/example/demo/controller/HotelController.java
+++ src/main/java/com/example/demo/controller/HotelController.java
@@ -3,13 +3,19 @@ import com.example.demo.model.Hotel;
 import com.example.demo.model.HotelRepository;
 import com.example.demo.model.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Collections;
+
+
 @RestController
 @CrossOrigin("http://localhost:4200")
 public class HotelController {
+
+
   @Autowired
   private HotelService hotelService;
   @Autowired
@@ -24,7 +30,13 @@ public class HotelController {
 
   public void printMeHotel(){
     hotel = this.hotelService.getHotelByName("Bosna Hotel");
+    System.out.println("hotel price is: " + hotel.getPrice());
     System.out.println("Hotel City:" + hotel.getCity());
+
+
+    List<Hotel> hotels_within_price_range = filter_getHotelWithinPriceRange("80");
+
+
   }
 
   @GetMapping(path="/hotels")
@@ -32,4 +44,37 @@ public class HotelController {
     System.out.println("Objects are ");
     return hotelRepository.findAll();
   }
+
+  @GetMapping(path="/hotels?price")
+  public @ResponseBody Iterable<Hotel> getHotelWithinPriceRange(@RequestParam String price) {
+    System.out.println("Objects are ");
+
+    List<Hotel> hotels_within_price_range = filter_getHotelWithinPriceRange(price);
+    Iterable<Hotel> return_hotels_within_price_range = hotels_within_price_range;
+
+    return return_hotels_within_price_range;
+  }
+
+  //-----Filtering----//
+  private List<Hotel> filter_getHotelWithinPriceRange(String price)
+  {
+    int hotel_price = Integer.parseInt(price);
+    List<Hotel> hotels_within_price_range = new ArrayList<Hotel>();
+    for(Hotel hotel: hotelService.getHotels())
+    {
+
+      if(hotel.getPrice() <= hotel_price)
+      {
+        hotels_within_price_range.add(hotel);
+        System.out.println("hotel name is: " + hotel.getName() + " and price is " + hotel.getPrice());
+      }
+    }
+
+    Collections.sort(hotels_within_price_range);
+    for(Hotel hotel_sorted: hotels_within_price_range)
+      System.out.println("hotel is: " + hotel_sorted.getName() + " and price is " + hotel_sorted.getPrice());
+
+    return hotels_within_price_range;
+  }
+
 }
