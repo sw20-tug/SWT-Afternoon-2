6fbeca0e13766cf2d9ff14ead92268612d3c095b
Aleksandar Stojicic <stojicic@student.tugraz.at>
2020-03-29 20:07:42 +0200

    HOTEL_002A Controller sends hotels within priceRange [MK,AS]

---
+++ src/main/java/com/example/demo/controller/Filters.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+public enum Filters {
+  PRICE,
+  PARKING,
+  RESTAURANT,
+  PETS_ALLOWED,
+  NON_SMOKING_ROOMS,
+  SWIMMING_POOL,
+  BEACHFRONT,
+  AIR_CONDITIONING,
+  FREE_WIFI,
+  SAUNA,
+  FITNESS,
+  DISTANCE_FROM_CENTER,
+  RATING
+}
--- src/main/resources/frontend/frontend-app/src/app/service/http-client.service.ts
+++ src/main/resources/frontend/frontend-app/src/app/service/http-client.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
 import {Hotel} from "../components/hotel-list/hotel.model";
+import {catchError, retry} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,17 @@ import {Hotel} from "../components/hotel-list/hotel.model";
 export class HttpClientService {
 
   private usersUrl: string;
-
+  private retryCount: number;
+  private httpOptions: any;
   constructor(private http: HttpClient) {
+    this.httpOptions = {
+      headers: new HttpHeaders()
+    };
+    this.httpOptions.headers.append('Access-Control-Allow-Headers', '*');
+    this.httpOptions.headers.append('Access-Control-Allow-Methods', '*');
+    this.httpOptions.headers.append('Access-Control-Allow-Origin', '*');
     this.usersUrl = 'http://localhost:8080/';
+    this.retryCount = 1;
   }
   public findAll(): Observable<Hotel[]> {
     return this.http.get<Hotel[]>(this.usersUrl+ '/hotels');
@@ -20,15 +29,31 @@ export class HttpClientService {
   public save(hotel: Hotel) {
     return this.http.post<Hotel>(this.usersUrl, Hotel);
   } */
+
   public getHotelWithinPriceRange(price: number): Observable<Hotel[]>
   {
-    return this.http.get<Hotel[]>(this.usersUrl + '/hotel?price=' + price);
+    return this.http.get<Hotel[]>(this.usersUrl+ '/hotel?price=' + price);
+
   }
 
-  public getHotelWithActivities(activities: string[]): Observable<Hotel[]>
+  public getHotelWithActivities(activities: string[]): Observable<any>
   {
-    return this.http.get<Hotel[]>(this.usersUrl + '/activities=' + activities);
+    return this.http.get<Hotel[]>(this.usersUrl + '/activities=' + activities, this.httpOptions) .pipe(
+      retry(this.retryCount),
+      catchError(this.errorHandler)
+    );
   }
+
+  public errorHandler(error) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
+
 
 
 }
