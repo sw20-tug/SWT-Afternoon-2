889aa65b0c472e88c8a470d092585a3b281e2fc8
Aleksandar Stojicic <stojicic@student.tugraz.at>
2020-04-05 15:48:57 +0200

    HOTEL_002 controller is ready to receive all parameters from fronted [MK, AS]

---
+++ src/main/java/com/example/demo/controller/Categories.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+public class Categories {
+
+  private Hotels hotel_inside_category;
+  public Categories(Hotels hotel_inside_category_){
+    this.hotel_inside_category = hotel_inside_category_;
+  }
+
+}
---
+++ src/main/java/com/example/demo/controller/Locations.java
@@ -0,0 +1,7 @@
+package com.example.demo.controller;
+
+public enum Locations {
+    GRAZ,
+    VIENNA,
+    SALZBURG
+}
