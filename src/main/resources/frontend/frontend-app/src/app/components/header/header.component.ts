4d7963cb9f7ce73a32f9e5a75da7163becbd8290
tarikbuza <buzatarik06@gmail.com>
2020-05-10 15:49:36 +0200

    HOTEL_009 Edited hotel detail page [BT, MS]

--- src/main/resources/frontend/frontend-app/src/app/components/header/header.component.ts
+++ src/main/resources/frontend/frontend-app/src/app/components/header/header.component.ts
@@ -8,9 +8,14 @@ import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 export class HeaderComponent implements OnInit {
   @Output() searchHeaderEvent = new EventEmitter<string>();
   @Input() public searchText: string = '';
+  @Input() public isSearchboxVisible: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(x) {
+    console.log(x)
   }
 
   searchBoxEvent($event){
