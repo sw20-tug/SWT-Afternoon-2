66367652f97005631307d682ebda8beccc618876
amelak9 <amela.kurtic@student.tugraz.at>
2020-05-20 20:06:33 +0200

    HOTEL_010 added controller functions [KA,RS]

--- src/main/java/com/hotel/repository/CommentRepository.java
+++ src/main/java/com/hotel/repository/CommentRepository.java
@@ -2,10 +2,12 @@ package com.hotel.repository;
 
 import com.hotel.model.Comment;
 import com.hotel.model.Hotel;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,5 +19,17 @@ public interface CommentRepository extends CrudRepository<Comment, Long> {
 
   @Query("FROM Comment WHERE hotel_id = :hotel_id")
   List <Comment> findCommentsByHotelId(@Param("hotel_id")long hotel_id);
+
+  @Query(
+    value = "SELECT id FROM swt.comment ORDER BY id DESC LIMIT 1",
+    nativeQuery = true)
+  Integer findLastId();
+
+  @Transactional
+  @Modifying
+  @Query(
+    value = "INSERT INTO swt.comment(id, comment, user_name, rate, hotel_id)",
+    nativeQuery = true)
+  void  insertNewHotels(@Param("id") long id, @Param("comment") String comment,  @Param("user_name") String user_name, @Param("rate") int rate, @Param("hotel_id") long hotel_id);
 }
 
