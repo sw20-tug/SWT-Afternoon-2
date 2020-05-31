c6e564c9db65e0977545670731be466e27826ac8
Mirza K <mirza.kabiljagic@myprofitsee.com>
2020-05-31 21:03:25 +0200

    HOTEL_023 Add TC to check if categories are initialized properly

--- src/main/resources/frontend/frontend-app/src/app/components/hotel-list/hotel-list.component.spec.ts
+++ src/main/resources/frontend/frontend-app/src/app/components/hotel-list/hotel-list.component.spec.ts
@@ -2,18 +2,13 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HotelListComponent } from './hotel-list.component';
 import { HttpClient, HttpHandler } from '@angular/common/http';
-import {HttpClientTestingModule} from "@angular/common/http/testing";
-import {TranslateModule} from "@ngx-translate/core";
-import {RouterTestingModule} from "@angular/router/testing";
+
 describe('HotelListComponent', () => {
   let component: HotelListComponent;
   let fixture: ComponentFixture<HotelListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule, HttpClientTestingModule, TranslateModule.forRoot()
-      ],
       declarations: [ HotelListComponent ],
       providers: [
         HttpClient,
